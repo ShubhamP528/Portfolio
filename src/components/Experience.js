956d09e51b6ccd81fb49c7d06f698cdd38269570
Shubham Prajapati <shubham528prajapati@gmail.com>
2025-02-15 07:11:16 +0530

    new change and added new project

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -136,42 +136,6 @@ const Experience = () => {
             </p>
           </div>
         </div>
-        {/* <div className="rounded-xl bg-gray shadow-md dark:bg-gray-100 dark:shadow-2xl mx-auto flex w-full max-w-4xl flex-col justify-between gap-4 p-8 md:flex-row md:gap-8">
-          <div className="max-md:order-1 md:w-1/4">
-            <img
-              alt="Upwork logo"
-              loading="lazy"
-              width="102"
-              height="28"
-              decoding="async"
-              data-nimg="1"
-              className="max-w-[120px]"
-              src="https://sagarshah.dev/_next/static/media/logo-upwork.5f1fcd5c.svg"
-              style={{ color: "transparent" }}
-            ></img>
-          </div>
-          <div className="flex flex-col gap-4 max-md:order-3 md:w-2/4">
-            <p className="text-lg md:text-xl font-semibold text-gray-900">
-              Independent Freelancer
-            </p>
-            <ul className="flex list-disc flex-col gap-2 md:gap-1">
-              <li className="text-normal text-base">
-                Worked for various clients like Fiskil, Shosho, Crowe MacKay
-                LLP.
-              </li>
-              <li className="text-normal text-base">
-                Worked with a variety of technologies, including React, Next.js,
-                Typescript, Express.js, PostgreSQL, Tailwindcss, Mui, Firebase,
-                Storybook, Cypress, and others.
-              </li>
-            </ul>
-          </div>
-          <div className="max-md:order-2 md:w-1/4">
-            <p className="text-base text-gray-700 md:text-right">
-              Nov 2021 - Present
-            </p>
-          </div>
-        </div> */}
       </div>
     </section>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -268,6 +268,85 @@ const Projects = () => {
             </a>
           </div>
         </div>
+        <div className="rounded-xl bg-gray shadow-md dark:bg-gray-100 dark:shadow-2xl mx-auto flex w-full max-w-6xl flex-col md:flex-row">
+          <div className="flex items-center justify-center border-gray-100 bg-gray-50 p-8 dark:bg-gray-200 max-md:rounded-t-xl md:w-1/2 lg:p-12 md:rounded-l-xl md:border-r md:order-last">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://linkbreeze.netlify.app/"
+            >
+              <img
+                alt="Shopping App"
+                loading="lazy"
+                width="1711"
+                height="1141"
+                decoding="async"
+                data-nimg="1"
+                className="rounded-xl shadow-lg transition-transform duration-500 md:hover:scale-105"
+                style={{ color: "transparent", objectFit: "cover" }}
+                src="https://res.cloudinary.com/dgsjppp4a/image/upload/v1739583370/mbjdyotqrcb9wq502ely.png"
+              ></img>
+            </a>
+          </div>
+          <div className="flex flex-col gap-6 p-8 md:w-1/2 lg:p-12 md:order-first">
+            <p className="text-lg md:text-xl font-semibold text-gray-900">
+              Link Breeze
+            </p>
+            <p className="text-normal text-base">
+              Experience effortless link management with Link Breeze! ✨ Shorten
+              URLs instantly, track analytics seamlessly, and optimize your
+              online presence—all in one place. Whether you're a marketer,
+              content creator, or just love simplicity, Link Breeze makes link
+              sharing smarter and more efficient. Try it now and take control of
+              your links!
+            </p>
+            <div className="flex flex-wrap gap-2">
+              <div className="flex items-center justify-center rounded-xl bg-gray-200 px-5 py-1">
+                <p className="text-normal text-sm font-medium">Express</p>
+              </div>
+              <div className="flex items-center justify-center rounded-xl bg-gray-200 px-5 py-1">
+                <p className="text-normal text-sm font-medium">React JS</p>
+              </div>
+
+              <div className="flex items-center justify-center rounded-xl bg-gray-200 px-5 py-1">
+                <p className="text-normal text-sm font-medium">JWT Auth</p>
+              </div>
+
+              <div className="flex items-center justify-center rounded-xl bg-gray-200 px-5 py-1">
+                <p className="text-normal text-sm font-medium">Tailwind</p>
+              </div>
+              <div className="flex items-center justify-center rounded-xl bg-gray-200 px-5 py-1">
+                <p className="text-normal text-sm font-medium">Javascipt</p>
+              </div>
+              <div className="flex items-center justify-center rounded-xl bg-gray-200 px-5 py-1">
+                <p className="text-normal text-sm font-medium">MongoDB</p>
+              </div>
+            </div>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="self-start rounded-lg p-1.5 hover:bg-gray-50 [&_svg]:stroke-gray-500"
+              href="https://linkbreeze.netlify.app/"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-external-link"
+              >
+                <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                <polyline points="15 3 21 3 21 9"></polyline>
+                <line x1="10" x2="21" y1="14" y2="3"></line>
+              </svg>
+            </a>
+          </div>
+        </div>
       </div>
     </section>
   );
